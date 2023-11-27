6bfb13fbd7b94341f2198ecc99ace7537c1f486e
ReformedTheo <lopes.theo22@gmail.com>
2023-11-27 19:21:27 -0300

    initial commit

---
+++ broker-service/cmd/api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "8000"
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+
+	log.Printf("Starting Broker on port 8000 %s\n", webPort)
+
+	//define http server
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	//start the server
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
